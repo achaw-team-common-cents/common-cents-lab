fdcc5e84cdb7c1eab84fe4078e6d5caf5f8699fd
Matt Gainer <matgainer@gmail.com>
2016-02-13 16:37:46 -0800

    made form for changing of the questions on the accounts form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :accounts
+  resources :questions
   namespace :api do
     namespace :v1 do
       resources :accounts
