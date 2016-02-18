6e10a06a61c0e2595e336f28f206a69d1e1022ee
Anthony Wu <anthonylongwu19@gmail.com>
2016-02-17 16:27:40 -0800

    finished view pages, added model methods for calculations including percentage

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,5 +7,13 @@ class Account < ActiveRecord::Base
     return savings + (pay_frequency.paid_per_month * income) - (3 * expenses)
   end
 
+  def calculate_percentage
+    annual_income = 12 * (pay_frequency.paid_per_month * income)
+    return (amount_needed / annual_income * 100).round(2)
+  end
+
+  def calculate_weekly
+    return amount_needed / 104
+  end
 
 end
