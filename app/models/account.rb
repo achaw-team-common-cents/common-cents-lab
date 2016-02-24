b12fb354d80197f5d9cb8740cecb8f700fb7c9dc
Matt Gainer <matgainer@gmail.com>
2016-02-24 14:39:20 -0800

    added code to generate csv file from data

--- app/models/account.rb
+++ app/models/account.rb
@@ -16,4 +16,12 @@ class Account < ActiveRecord::Base
     return amount_needed / 104
   end
 
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << ["Date Added", "Savings ($)", "Monthly Expenses ($)", "Pay Frequency", "Average Income Per Paycheck ($)", "Amount Needed to Cover 3 Months ($)", "Difficulty Finding a New Job", "View Type"]
+      all.each do |account|
+        csv << [account.created_at, account.savings, account.expenses, account.pay_frequency.name, account.income, account.amount_needed, account.difficulty.name, account.view_type]
+      end
+    end
+  end
 end
